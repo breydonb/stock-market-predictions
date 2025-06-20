dd36d4a4157e167eddf52ea08e36e7422832a6ec
breydonb <breydonb1113@gmail.com>
2025-06-20 18:36:21 -0500

    resolving merge issue

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,10 +1,29 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import type { ViteUserConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config'
 
-export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	build: {
-		outDir: 'dist'
-	}
+const mode = import.meta.env.VITE_ENV_MODE;
+
+export default defineConfig(({ mode }) => {
+	const isDev = mode == 'dev-mode'
+	const isTest = mode == 'unit-test';
+	const isProduction = mode == 'production';
+	return  {
+		plugins: [tailwindcss(), sveltekit()],
+		define: {
+			__APP_ENV__: JSON.stringify(mode),
+		},
+		build: {
+			minify: isProduction,
+			outDir: "/dist"
+		}
 });
+	
+function createPlugin(mode: string) {
+	const plugins = [sveltekit(), tailwindcss()];
+
+}
+
+
+
